b8e71da2d406ef87307ac0c19df5d20bdc95065d
Thomas Dehennault <69753731+AVNThomas@users.noreply.github.com>
2022-10-09 01:04:40 +0200

    {ADD} added router

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const app = require('express');
-
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 const server = app();
+const fs = require('fs');
+const { callbackify } = require('util');
 
 PORT = process.env.PORT || 3000;
 
+const pages = new Map();
+
+const files = fs.readdirSync('./src/pages');
+const jsFiles = files.filter(f => f.split('.').pop() === 'js');
+jsFiles.forEach(jsFile => {
+    const page = require(`./src/pages/${jsFile}`);
+    pages.set(page.name, page);
+    console.log(`Loaded file: ${jsFile}`);
+});
+
 server.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World!');
+});
+
+pages.forEach((pages) => {
+    console.log('loaded page: ' + pages.name);
+    server.get(`/${pages.name}`, pages.run);
 });
 
 server.listen(PORT, () => {
